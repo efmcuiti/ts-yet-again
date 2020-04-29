bee6c61cbab8df02bb3879577b2bb1258c1bfa7a
Edison Mendez <efmcuiti@gmail.com>
2020-04-28 21:10:35 -0500

    Add first function.

--- app.ts
+++ app.ts
@@ -1,1 +1,19 @@
 console.log('Put your freaking code here');
+
+// TypeScript types.
+// TS can help us up to development time.
+
+/**
+ * Adds two basic numbers.
+ * @param {number} firsty - The first element to do math.
+ * @param {number} secondy - The second element to do math.
+ * @return {number} The math result from parameters.
+ */
+function add(firsty: number, secondy: number) {
+  return firsty + secondy;
+};
+
+const number1 = 12;
+const number2 = 5;
+
+console.log('Faquin result: ', add(number1, number2));
